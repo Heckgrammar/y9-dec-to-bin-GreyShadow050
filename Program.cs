7ea70f59ca9a8c2cee476029de5a2b8508f3e946
GreyShadow050 <21ZBrooks@heckgrammar.co.uk>
2024-07-02 15:14:53 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,31 +11,20 @@ namespace Y9_DEC_TO_BIN_SKELETON
     {
         static void Main(string[] args)
         {
-            //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
-            string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
-
-            //MAIN:  NUMBER CONVERSION PROGRAM
-            Console.WriteLine(numberConversion(926,2));
-            string stringS = numberConversion(926, 2);
-            //CODE GOES HERE
-            Console.WriteLine(StringInString("I want"," a lot of food"));
-        }
-
-        //static void means the function will not return a value so it does not need a data type 
-        //...this function DOES return a value so the method must have a data type
-        static string numberConversion(int number, int numberbase)
-        {
-            //CODE GOES HERE
-            string result= Convert.ToString(number+numberbase);
-            return result; //REMOVE THE RED LINE!   
-        }
-
-        static string StringInString(string string1, string string2)
-        {
-            string result = string1 + string2;
-            return result;
+            Console.WriteLine("Input your dinary number");
+            int denary=Convert.ToInt32(Console.ReadLine());
+            int[] binary = new int[16];
+            for (int i = 0; denary > 0; i++)
+            {
+                binary[i] = denary % 2;
+                denary= denary / 2;
+            }
+            int step = binary.Length - 1;
+            for (int i = step; i < 0; i--)
+            {
+                Console.Write(binary[i]);
+            }
+            Console.Write("");
         }
     }
 }
