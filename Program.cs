b5a40361513829bc46d53d081f3f61f40d583840
GreyShadow050 <21ZBrooks@heckgrammar.co.uk>
2024-07-03 14:10:35 +0100

    .

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace Y9_DEC_TO_BIN_SKELETON
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input your dinary number");
+            Console.WriteLine("Input your denary number");
             int denary=Convert.ToInt32(Console.ReadLine());
-            int[] binary = new int[16];
+            int[] binary = new int[8];
             for (int i = 0; denary > 0; i++)
             {
                 binary[i] = denary % 2;
                 denary= denary / 2;
             }
-            int step = binary.Length - 1;
-            for (int i = step; i < 0; i--)
-            {
+            int step = binary.Length -1;
+            for (int i = step; i >= 0; i--)
+            {   
                 Console.Write(binary[i]);
             }
             Console.Write("");
